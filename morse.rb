a42c314443d7535d84451d39cde781441585c9cb
ichala <alijandhd@gmail.com>
2022-07-13 14:01:35 +0100

    Fix Linters

--- morse.rb
+++ morse.rb
@@ -27,28 +27,25 @@
   '--..' => 'Z'
 }
 
-#Create a method to decode a Morse code character, takes a string parameter, and return the corresponding character in uppercase (e.g. decode_char(".-") returns "A").
-
 def decode_char(char)
-    @morse[char]
+  @morse[char]
 end
-
-#Create a method to decode an entire word in Morse code, takes a string parameter, and return the string representation. Every character in a word will be separated by a single space (e.g. decode_word("-- -.--") returns "MY").
 
 def decode_word(word)
-    final_word = ''
-    word.split.each do |char|
-        final_word += decode_char(char)
-    end
-    final_word
+  final_word = ''
+  word.split.each do |char|
+    final_word += decode_char(char)
+  end
+  final_word
 end
-
-#Create a method to decode the entire message in Morse code, takes a string parameter, and return the string representation. Every word will be separated by 3 spaces (e.g.
 
 def decode_message(msg)
-    final_msg = ''
-    msg.split('   ').each do |word|
-        final_msg += decode_word(word) + ' '
-    end
-    final_msg.chop
+  final_msg = ''
+  msg.split('   ').each do |word|
+    final_msg += decode_word(word)
+    final_msg += ' '
+  end
+  final_msg.chop
 end
+
+puts decode_message('-- -.--   -. .- -- .')
